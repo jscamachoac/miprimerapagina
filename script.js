be29409e6575e5bfd87ae03b93dcd6df27242b70
jscamachoac <johancamacho583@gmail.com>
2023-11-21 19:32:58 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+//alert("Hola este es mi Javascript");
+
+let contenidoTitulo = "Mi pagina";
+
+let titulo = document.querySelector(".logo .fuente-acento");
+titulo.innerHTML = contenidoTitulo;
+
+//CONDICIONALES
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+if(textoTitulo == "Mi pagina") {
+    titulo.innerHTML = "Mi pagina web";
+}
+
+
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  parrafo.innerHTML = "";
+
+  if (nombre.value.length < 4) {
+    warnings += `El nombre debe contener más de 4 carcateres`;
+    valido = false;
+  }
+
+  if (!valido) {
+    parrafo.innerHTML = warnings;
+  } else {
+    parrafo.innerHTML = "Enviado";
+  }
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+  } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  }
+});
